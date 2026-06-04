8962a84192b490805ec05e9abba402b3175a06ec
maulanaikhsan55 <maulanamuhammadikhsamxap2@gmail.com>
2026-06-04 20:31:55 +0700

    feat: replace SweetAlert with modal component in master products page

--- resources/views/master/products/index.blade.php
+++ resources/views/master/products/index.blade.php
@@ -5,7 +5,7 @@
 
 @section('styles')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<link rel="stylesheet" href="{{ asset('css/modal.css') }}?v={{ time() }}">
 <style>
     * { font-family: 'Plus Jakarta Sans', sans-serif; }
     .main-content { background-color: transparent !important; box-shadow: none !important; border: none !important; padding: 0 !important; }
